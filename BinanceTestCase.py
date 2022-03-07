f500a422ed2e8e81259c47d2921a9220ecc1f8b2
cengisan <77883086+cengisan@users.noreply.github.com>
2022-03-07 22:28:04 +0300

    Update BinanceTestCase.py

--- BinanceTestCase.py
+++ BinanceTestCase.py
@@ -3,7 +3,6 @@ import unittest
 import HtmlTestRunner
 import time
 
-
 class BitcoinTestCase(unittest.TestCase):
     driver = None
 
@@ -19,9 +18,8 @@ class BitcoinTestCase(unittest.TestCase):
         driver.find_element_by_name("q").send_keys("Binance")
         driver.find_element_by_name("btnK").click()
 
-        link = self.driver.find_element_by_partial_link_text(
-            "Bitcoin, Ether ve Altcoin Al/Sat | Kripto Para Borsası | Binance")
-        link.click()
+        driver.find_element_by_partial_link_text(
+            "Bitcoin, Ether ve Altcoin Al/Sat | Kripto Para Borsası | Binance").click()
         time.sleep(3)
 
         driver.find_element_by_xpath("//*[@id='__APP']/div/header/div[4]/div[2]/div[2]").click()
@@ -33,7 +31,13 @@ class BitcoinTestCase(unittest.TestCase):
         driver.find_element_by_xpath("// *[ @ id = 'header_menu_ba-tableMarkets'] / div").click()
         time.sleep(3)
 
-        driver.find_element_by_id("markets_main_search").send_keys("Bitcoin")
+        driver.find_element_by_xpath("//*[@id='markets_main_search']").send_keys("Bitcoin")
+        time.sleep(3)
+        
+        driver.find_element_by_xpath("//*[@id='tabContainer']/div[2]/div[2]/div/div[2]/div[1]/div/div[6]/a").click()
+        time.sleep(5)
+
+        time.sleep(5)
 
     @classmethod
     def tearDownClass(cls):
@@ -41,8 +45,6 @@ class BitcoinTestCase(unittest.TestCase):
         cls.driver.quit()
 
 
-'''if __name__ == "__main__":
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output="C:/Users/ASUS/AppData/Roaming/JetBrains"
-                                                                  "/PyCharmCE2021.3/scratches/selenium/Reports"))'''
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output="C:/Users/ASUS/AppData/Roaming/JetBrains"
+                                                                  "/PyCharmCE2021.3/scratches/selenium/Reports"))
