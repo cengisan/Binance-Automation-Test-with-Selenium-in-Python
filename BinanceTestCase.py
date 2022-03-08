95904657b26918f3680f50ef27b416fc281aa5d6
cengisan <77883086+cengisan@users.noreply.github.com>
2022-03-08 17:26:11 +0300

    Update BinanceTestCase.py

--- BinanceTestCase.py
+++ BinanceTestCase.py
@@ -3,6 +3,7 @@ import unittest
 import HtmlTestRunner
 import time
 
+
 class BitcoinTestCase(unittest.TestCase):
     driver = None
 
@@ -14,29 +15,27 @@ class BitcoinTestCase(unittest.TestCase):
 
     def test_entrance_binance(self):
         driver = self.driver
+
         driver.get("https://google.com")
         driver.find_element_by_name("q").send_keys("Binance")
         driver.find_element_by_name("btnK").click()
-
         driver.find_element_by_partial_link_text(
             "Bitcoin, Ether ve Altcoin Al/Sat | Kripto Para Borsası | Binance").click()
         time.sleep(3)
 
         driver.find_element_by_xpath("//*[@id='__APP']/div/header/div[4]/div[2]/div[2]").click()
         time.sleep(3)
-
         driver.find_element_by_xpath("// *[ @ id = 'fiatlngdialog_ba-Currency-USD']").click()
         time.sleep(3)
 
+    def test_open_btc_page(self):
+        driver = self.driver
+
         driver.find_element_by_xpath("// *[ @ id = 'header_menu_ba-tableMarkets'] / div").click()
         time.sleep(3)
-
         driver.find_element_by_xpath("//*[@id='markets_main_search']").send_keys("Bitcoin")
         time.sleep(3)
-        
         driver.find_element_by_xpath("//*[@id='tabContainer']/div[2]/div[2]/div/div[2]/div[1]/div/div[6]/a").click()
-        time.sleep(5)
-
         time.sleep(5)
 
     @classmethod
